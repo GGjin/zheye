4c9bf3fde6ea1dbdac86a85c1daca9a2f92fd9ea
Jinyu.Guo3 <gg.jin.yu@gmail.com>
2022-09-16 18:31:00 +0800

    使用vue3新版的v-model双向绑定数据和控件

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,112 @@
 <template>
-  <div class="container">
-    <global-header :user="currentUser" />
-    <form action="">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-input :rules="emailRules" />
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" v-model="emailRef.val" @blur="validateEmail" />
-        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" />
-      </div>
-    </form>
-  </div>
+    <div class="container">
+        <global-header :user="currentUser" />
+        <form action="">
+            <div class="mb-3">
+                <label class="form-label">邮箱地址</label>
+                <validate-input :rules="emailRules" v-model="emailVal" />
+                {{ emailVal }}
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" v-model="emailRef.val" @blur="validateEmail" />
+                <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">密码</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+        </form>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { ColumnProps } from "./components/ColumnList.vue";
-import GlobalHeader, { UserProps } from "./components/GlobalHeader.vue";
-import ValidateInput, { RelesProp } from "./components/ValidateInput.vue";
+import { defineComponent, reactive, ref } from 'vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ColumnProps } from './components/ColumnList.vue';
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue';
+import ValidateInput, { RelesProp } from './components/ValidateInput.vue';
 const currentUser: UserProps = {
-  isLogin: true,
-  name: "GG",
+    isLogin: true,
+    name: 'GG',
 };
-const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: "test1的专栏",
-    description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
-  },
-  {
-    id: 2,
-    title: "test2的专栏",
-    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
-  },
-  {
-    id: 2,
-    title: "test2的专栏",
-    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
-    // avatar:
-    //   'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
-  },
-  {
-    id: 2,
-    title: "test2的专栏",
-    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
-    avatar:
-      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100",
-  },
+    {
+        id: 1,
+        title: 'test1的专栏',
+        description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+    },
+    {
+        id: 2,
+        title: 'test2的专栏',
+        description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+    },
+    {
+        id: 2,
+        title: 'test2的专栏',
+        description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+        // avatar:
+        //   'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+    },
+    {
+        id: 2,
+        title: 'test2的专栏',
+        description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
+        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+    },
 ];
 
 export default defineComponent({
-  name: "App",
-  components: {
-    // ColumnList,
-    GlobalHeader,
-    ValidateInput,
-  },
-  setup() {
-    const emailRules: RelesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
+    name: 'App',
+    components: {
+        // ColumnList,
+        GlobalHeader,
+        ValidateInput,
+    },
+    setup() {
+        const emailVal = ref('GG');
+        const emailRules: RelesProp = [
+            { type: 'required', message: '电子邮箱地址不能为空' },
+            { type: 'email', message: '请输入正确的电子邮箱格式' },
+        ];
 
-    const emailRef = reactive({
-      val: '',
-      error: false,
-      message: ''
-    })
+        const emailRef = reactive({
+            val: '',
+            error: false,
+            message: '',
+        });
 
-    const validateEmail = () => {
-      if (emailRef.val.trim() === '') {
-        emailRef.error = true
-        emailRef.message = 'can not be empty'
-      } else if (!emailReg.test(emailRef.val)) {
-        emailRef.error = true
-        emailRef.message = 'should be valid message'
-      }
-    }
-    return {
-      list: testData,
-      currentUser,
-      emailRef,
-      validateEmail,
-      emailRules
-    };
-  },
+        const validateEmail = () => {
+            if (emailRef.val.trim() === '') {
+                emailRef.error = true;
+                emailRef.message = 'can not be empty';
+            } else if (!emailReg.test(emailRef.val)) {
+                emailRef.error = true;
+                emailRef.message = 'should be valid message';
+            }
+        };
+        return {
+            list: testData,
+            currentUser,
+            emailRef,
+            validateEmail,
+            emailRules,
+            emailVal,
+        };
+    },
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/ValidateInput.vue
+++ src/components/ValidateInput.vue
@@ -1,55 +1,63 @@
 <template>
     <div class="validate-input-container pb-3">
-        <input type="text" class="form-control" :class="{'is-invalid':inputRef.error}" v-model="inputRef.val"
-            @blur="validateInput" />
-        <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
+        <input
+            type="text"
+            class="form-control"
+            :class="{ 'is-invalid': inputRef.error }"
+            :value="inputRef.val"
+            @input="updateValue"
+            @blur="validateInput"
+        />
+        <span v-if="inputRef.error" class="invalid-feedback">{{ inputRef.message }}</span>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, reactive } from 'vue'
+import { defineComponent, PropType, reactive } from 'vue';
 
-const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 interface RuleProp {
     type: 'required' | 'email';
     message: string;
 }
-export type RelesProp = RuleProp[]
+export type RelesProp = RuleProp[];
 export default defineComponent({
     props: {
-        rules: Array as PropType<RelesProp>
+        rules: Array as PropType<RelesProp>,
+        modelValue: String,
     },
-    setup(props) {
+    setup(props, context) {
         const inputRef = reactive({
-            val: '',
+            val: props.modelValue || '',
             error: false,
-            message: ''
-        })
+            message: '',
+        });
+        const updateValue = (e: KeyboardEvent) => {
+            const targetValue = (e.target as HTMLInputElement).value;
+            inputRef.val = targetValue;
+            context.emit('update:modelValue', targetValue);
+        };
         const validateInput = () => {
             if (props.rules) {
                 const allPassed = props.rules.every(rule => {
-                    let passed = true
-                    inputRef.message = rule.message
+                    let passed = true;
+                    inputRef.message = rule.message;
                     switch (rule.type) {
                         case 'required':
-                            passed = (inputRef.val.trim() !== '')
-                            break
+                            passed = inputRef.val.trim() !== '';
+                            break;
 
                         case 'email':
                             passed = emailReg.test(inputRef.val);
-                            break
+                            break;
                     }
-                    return passed
-                })
-                inputRef.error = !allPassed
+                    return passed;
+                });
+                inputRef.error = !allPassed;
             }
-        }
-        return { inputRef, validateInput }
-    }
-})
+        };
+        return { inputRef, validateInput, updateValue };
+    },
+});
 </script>
-
-<style scoped>
-
-</style>
